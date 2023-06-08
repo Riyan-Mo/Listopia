aa92d46dc2b6fc5224ec3130a1222b297ce76181
Riyan911 <Riyan8308151899@gmail.com>
2023-06-08 15:55:54 +0530

    Initial commit

---
+++ dist/index.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunklistopia"] = self["webpackChunklistopia"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("Lmao");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9saXN0b3BpYS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkxtYW9cIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
